c1c4a1d676a4752b3d467296923247125a4d6afa
Managam & Vania <managam@vania.wedding>
2026-05-23 11:26:23 +0700

    fix: trailingSlash false to stop slash appended to redirect destinations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',          // static export for Cloudflare Pages
-  trailingSlash: true,
+  trailingSlash: false,
   images: {
     unoptimized: true,       // required for static export
     remotePatterns: [
